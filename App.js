f1ea888e463673e63b7ee98071a756c4d1e6d681
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-02-25 19:06:44 +0530

    add script to run the project

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
-const heading2 = React.createElement("h1", { id: "title" }, "Heading 2");
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+const Title = () => {
+  return <h1 className="head">Namaste React using JSX</h1>;
+};
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>Namaste React Functional Component</h1>
+    </div>
+  );
+};
 
+const App = () => {
+  return <HeadingComponent />;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<App />);
